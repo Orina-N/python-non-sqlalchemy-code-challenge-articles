968bc885c1f557a1572a28896f7c5638521d14cb
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-05-24 22:15:57 +0300

    first commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,29 @@
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        self.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Cannot set attribute 'title'")
+
+    def _validate_title(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string")
+        if len(title) < 5 or len(title) > 50:
+            raise ValueError("Title must be between 5 and 50 characters inclusive")
+        return title
         
+
 class Author:
     def __init__(self, name):
         self.name = name
@@ -36,3 +56,10 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+Article.all = []
+author = Author("Carry Bradshaw")
+magazine_1 = Magazine("Vogue", "Fashion")
+magazine_2 = Magazine("AD", "Architecture & Design")
+article_1 = Article(author, magazine_1, "How to wear a tutu with style")
+article_2 = Article(author, magazine_2, "Dating life in NYC")
